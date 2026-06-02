cee09330ae56bf52e9018eb1e1ac30bf88b527f5
aminpy83 <aminpyss@gmail.com>
2026-06-02 16:49:05 +0330

    pdf_loader.py

---
+++ modules/pdf_loader.py
@@ -0,0 +1,7 @@
+from pymupdf import open
+
+def extract_text(pdf_path: str):
+    doc = open(pdf_path)
+    for num, txt in enumerate(doc):
+        yield (f'num: {num+1} \n \
+               {txt.get_text()}')
